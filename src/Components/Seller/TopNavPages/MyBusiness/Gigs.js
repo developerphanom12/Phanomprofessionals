632c35f9417feba1aa07808448b1705a14a655e4
developer <developer.phanomprofessionals@gmail.com>
2024-02-29 19:04:53 +0530

    newone

--- src/Components/Seller/TopNavPages/MyBusiness/Gigs.js
+++ src/Components/Seller/TopNavPages/MyBusiness/Gigs.js
@@ -14,10 +14,24 @@ export default function Gigs() {
             <Switch>
               <StyledInput type="checkbox" />
               <GlobalSlider />
-            </Switch>Accepting Custom orders
+            </Switch>
+            Accepting Custom orders
           </div>
         </div>
-        <div></div>
+        <div className="tab_button_area">
+          <div className="side_btn">
+            <button>ACTIVE</button>
+            <button>PENDING APPROVAL</button>
+            <button>REQUIRES MODIFICATION</button>
+            <button>DRAFT</button>
+            <button>DENIED</button>
+            <button>PAUSED</button>
+          </div>
+          <div className="create_btn">
+            <button>CREATE A NEW GIG</button>
+          </div>
+        </div>
+        <div className="gig_table">table</div>
       </div>
     </Root>
   );
@@ -35,6 +49,38 @@ const Root = styled.section`
         font-family: "macan", "helvetica neue", Helvetica, Arial, sans-serif;
         font-weight: 300;
         color: #404145;
+      }
+    }
+    .tab_button_area {
+      display: flex;
+      justify-content: space-between;
+      .side_btn {
+        display: flex;
+        gap: 10px;
+        padding: 10px;
+        color: #999;
+        font-size: 14px;
+        line-height: 42px;
+        font-weight: 400;
+        text-decoration: none;
+        text-transform: uppercase;
+        button {
+          border: none;
+          background-color: transparent;
+          color: #404145;
+          padding: 10px;
+        }
+      }
+      .create_btn {
+        button {
+          background-color: #1dbf73;
+          font-weight: 700;
+          color: #fff !important;
+          border: 1px solid transparent;
+          padding: 5px 10px;
+          font-size: 12px;
+          border-radius: 5px;
+        }
       }
     }
   }
