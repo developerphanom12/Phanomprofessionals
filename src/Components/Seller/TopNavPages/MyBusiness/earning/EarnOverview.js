75bc0fc36c4daf2a4605a6cf00b9875c21b34dff
developer <developer.phanomprofessionals@gmail.com>
2024-03-13 18:46:39 +0530

    api call

--- src/Components/Seller/TopNavPages/MyBusiness/earning/EarnOverview.js
+++ src/Components/Seller/TopNavPages/MyBusiness/earning/EarnOverview.js
@@ -4,15 +4,20 @@ import { HiQuestionMarkCircle } from "react-icons/hi";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { GrDocumentCsv } from "react-icons/gr";
 
-
 export default function EarnOverview() {
   const [show, setShow] = useState(false);
-  function toggleShowName(){
-    setShow(prevState => !prevState);
-  };
+  const [activity, setActivity] = useState(false);
+
+  function toggleShowName() {
+    setShow((prevState) => !prevState);
+  }
+
+  function toggleActivity() {
+    setActivity((prevState) => !prevState);
+  }
   return (
     <Root>
-      <div className="main_div"  onClick={() => setShow(!show)}>
+      <div className="main_div" >
         <div className="main_div_one">
           <div className="column_one">
             <h6>Available funds</h6>
@@ -94,43 +99,99 @@ export default function EarnOverview() {
 
         <div className="main_div_two">
           <div className="table_menubar">
-            <button  onClick={toggleShowName}>
+            <button onClick={toggleShowName}>
               <span>Date range</span>
-              <MdKeyboardArrowDown/>
+              <MdKeyboardArrowDown />
             </button>
-            <div className="show-content"> 
-              {show && <h1>Hello Gagan</h1>}
-            </div>
-            
-            <button>
+
+            <button onClick={toggleActivity}>
               <span>Acitivity</span>
               <MdKeyboardArrowDown />
             </button>
           </div>
 
+          {show && (
+            <div className="table_filter">
+              <div className="table_filter_header">
+                <h6 className="table_heading">Date range</h6>
+                {/ <button>close</button> /}
+              </div>
+              <div className="table_filter_body">
+                <select>
+                  <option>Select a month</option>
+                  <option>Mar 2024</option>
+                  <option>Feb 2024</option>
+                  <option>Jan 2024</option>
+                  <option>Dec 2023</option>
+                  <option>Nov 2023</option>
+                  <option>Oct 2023</option>
+                  <option>Sep 2023</option>
+                  <option>Aug 2023</option>
+                  <option>Jul 2023</option>
+                  <option>Jun 2023</option>
+                  <option>May 2023</option>
+                  <option>Apr Month</option>
+                </select>
+
+                <div className="date_maindiv">
+                  <div className="from_date">
+                    <h6>From</h6>
+                    <input type="date" placeholder="Date"></input>
+                  </div>
+                  <div className="to_date">
+                    <h6>To</h6>
+                    <input type="date" placeholder="Date"></input>
+                  </div>
+                </div>
+              </div>
+
+              <div className="table_filter_footer">
+                <div className="clear_btn">
+                  <a href="#">Clear all</a>
+                </div>
+                <div className="submit_btn">
+                  <a href="#">Apply</a>
+                </div>
+              </div>
+            </div>
+          )}
+
+          {activity && (
+            <div className="activity_div">
+              <div className="activity_div_header">
+                <h6 className="activity_heading">Activity</h6>
+              </div>
+
+              <div className="activity_footer">
+                <div className="submit_btn">
+                  <a href="#">Apply</a>
+                </div>
+              </div>
+            </div>
+          )}
+
           <div className="table_status">
-                  <div className="status_content">
-                    <span>Showing 0 results.</span>
-                  </div>
+            <div className="status_content">
+              <span>Showing 0 results.</span>
+            </div>
 
-                  <div className="email_tooltip">
-                    <button>
-                    <GrDocumentCsv />
-                      <span>Email activity report</span>
-                    </button>
-                  </div>
-
-                
+            <div className="email_tooltip">
+              <button>
+                <GrDocumentCsv />
+                <span>Email activity report</span>
+                <span className="tooltiptext">Your report will include data based on the date range and activity filters selected. Future payments will not appear in the report.</span>
+              </button>
+            </div>
           </div>
 
           <div className="table_body">
             <div className="table_body_header">
-                  <span>Date</span>
-                  <span>Activity</span>
-                  <span>Description</span>
-                  <span>From</span>
-                  <span>Other</span>
-                  <span>Amount</span>
+              <span>Date</span>
+              <span>Activity</span>
+              <span>Description</span>
+              <span>From</span>
+              <span>Other</span>
+              <span>Amount</span>
             </div>
           </div>
         </div>
@@ -358,13 +419,14 @@ const Root = styled.section`
   .main_div_two {
     margin-top: 40px;
     .table_menubar {
-      display:flex;
+      display: flex;
       button {
         border-radius: 4px;
         border: 1px solid #dadbdd;
-        padding:10px 17px;
-        background-color:#fff;
-        margin-right:20px;
+        padding: 10px 17px;
+        background-color: #fff;
+        margin-right: 20px;
+        position: relative;
         span {
           color: #404145;
           font-weight: 600;
@@ -375,58 +437,234 @@ const Root = styled.section`
       }
     }
 
-    .table_status{
-      margin-top:24px;
+    .table_status {
+      margin-top: 24px;
       display: flex;
       justify-content: space-between;
 
-      .status_content{
-        span{
+      .status_content {
+        span {
           color: #62646a;
-          font-size:16px;
-          font-weight:500;
-
-
+          font-size: 16px;
+          font-weight: 500;
         }
       }
-      .email_tooltip{
-        button{
+      .email_tooltip {
+        button {
           border:none;
           background:#fff;
-          svg{
-            color:green;
+          position:relative;
+          display:inline-block;
+          svg {
+            color: green;
             width: 20px;
-            height:27px;
+            height: 27px;
           }
-          span{
-          font-weight: 600;
-          margin-left: 8px;
-          color: #404145;
-          cursor: pointer;
-          font-size:16px;
+          span {
+            font-weight: 600;
+            margin-left: 8px;
+            color: #404145;
+            cursor: pointer;
+            font-size: 16px;
+          }
         }
+        button .tooltiptext {
+          visibility:hidden;
+          width: 260px;
+          font-size:15px;
+          font-weight:400;
+          background-color:#404145;
+          color:#fff;
+          text-align:left;
+          border-radius:6px;
+          padding:15px 15px;
+          position:absolute;
+          z-index:1;
+          bottom:125%;
+          left:0;
+          margin-left:-60px;
+          opacity:0;
+          transition:opacity 0.3s;
         }
-        
 
+        button .tooltiptext::after {
+          content: "";
+          position: absolute;
+          top: 100%;
+          left: 50%;
+          margin-left: -5px;
+          border-width: 5px;
+          border-style: solid;
+          border-color: #555 transparent transparent transparent;
+        }
+
+        button:hover .tooltiptext {
+          visibility: visible;
+          opacity: 1;
+        }
       }
     }
 
-    .table_body{
+    .table_body {
       margin-top: 24px;
       border: 1px solid #dadbdd;
       border-radius: 6px;
-      height:400px;
-      .table_body_header{
-        background-color:#fafafa;
-          display:flex;
-          padding:15px 20px;
-          span{
-            flex:1;
-            color:#404145;
+      height: 400px;
+      .table_body_header {
+        background-color: #fafafa;
+        display: flex;
+        padding: 15px 20px;
+        span {
+          flex: 1;
+          color: #404145;
+          font-size: 14px;
+          font-weight: 700;
+          text-align: center;
+        }
+      }
+    }
+  }
+
+  .table_filter {
+    width: 420px;
+    border: 1px solid #dadbdd;
+    padding: 24px 24px;
+    display: flex;
+    flex-direction: column;
+    background-color: #fff;
+    border-radius: 4px;
+    margin-top: 20px;
+    position: absolute;
+    .table_heading {
+      color: #222325;
+      font-size: 18px;
+      font-weight: 700;
+    }
+    .table_filter_body {
+      margin-top: 15px;
+      select {
+        width: 100%;
+        height: 45px;
+        border-radius: 4px;
+        border: 1px solid #dadbdd;
+        color: #b5b6ba;
+        margin-bottom: 24px;
+        option {
+          color: black;
+          padding: 20px 0;
+        }
+      }
+
+      .date_maindiv {
+        display: flex;
+        .from_date {
+          width: 100%;
+          margin-right: 12px;
+          h6 {
+            font-weight: 600;
             font-size: 14px;
-            font-weight: 700;
-            text-align:center;
+            margin-bottom: 8px;
           }
+          input {
+            width: 100%;
+            height: 45px;
+            border-radius: 4px;
+            border: 1px solid #dadbdd;
+            color: #b5b6ba;
+            margin-top: 5px;
+          }
+        }
+        .to_date {
+          width: 100%;
+          margin-left: 12px;
+          h6 {
+            font-weight: 600;
+            font-size: 14px;
+            margin-bottom: 8px;
+          }
+          input {
+            width: 100%;
+            height: 45px;
+            border-radius: 4px;
+            border: 1px solid #dadbdd;
+            color: #b5b6ba;
+            margin-top: 5px;
+          }
+        }
+      }
+    }
+
+    .table_filter_footer {
+      display: flex;
+      justify-content: space-between;
+      border-top: 1px solid #efeff0;
+      margin-top: 30px;
+      padding-top: 25px;
+      .clear_btn {
+        a {
+          text-decoration: none;
+          text-align: center;
+          cursor: pointer;
+          font-weight: 600;
+          color: #62646a;
+        }
+      }
+      .submit_btn {
+        a {
+          background-color: #1dbf73;
+          color: #fff;
+          padding: 10px 16px;
+          font-size: 14px;
+          text-decoration: none;
+          border-radius: 4px;
+          font-weight: 600;
+          text-align: center;
+          &:hover {
+            background-color: #19a463;
+          }
+        }
+      }
+    }
+  }
+
+  .activity_div {
+    width: 264px;
+    border: 1px solid #dadbdd;
+    padding: 24px 24px;
+    display: flex;
+    flex-direction: column;
+    background-color: #fff;
+    border-radius: 4px;
+    margin-top: 20px;
+    left: 14%;
+    position: absolute;
+    .activity_div_header {
+      .activity_heading {
+        color: #222325;
+        font-size: 18px;
+        font-weight: 700;
+      }
+    }
+    .activity_footer {
+      display: flex;
+      justify-content: flex-end;
+      border-top: 1px solid #efeff0;
+      margin-top: 30px;
+      padding-top: 25px;
+      .submit_btn {
+        a {
+          background-color: #1dbf73;
+          color: #fff;
+          padding: 10px 16px;
+          font-size: 14px;
+          text-decoration: none;
+          border-radius: 4px;
+          font-weight: 600;
+          text-align: center;
+          &:hover {
+            background-color: #19a463;
+          }
+        }
       }
     }
   }
